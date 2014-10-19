f4a7a33b8b6b0ed4337f24190b1e43b5c265f41f
OptimusCrime <thomasgautv@hotmail.com>
2014-10-20 00:28:01 +0200

    Build dynamic fetcher

--- index.php
+++ index.php
@@ -7,6 +7,13 @@
 */
 
 //
+// Some variables
+//
+
+define('BASE_DIR', dirname(__FILE__));
+define('TEMPLATE_DIR', BASE_DIR . '/templates/');
+
+//
 // Set headers
 //
 
@@ -16,7 +23,7 @@ header('Content-Type: text/html; charset=utf-8');
 // Include the libraries
 //
 
-require dirname(__FILE__) . '/vendor/autoload.php';
+require BASE_DIR . '/vendor/autoload.php';
 
 //
 // Class Loader (that loads the correct template)
@@ -43,6 +50,9 @@ class Loader {
         $this->template = new Smarty();
         $this->template->left_delimiter = '[[+'; 
         $this->template->right_delimiter = ']]';
+        $this->template->assign('TEMPLATE_DIR', TEMPLATE_DIR);
+        $this->template->assign('TOP_LEVEL_MENU', '');
+        $this->template->assign('SECOND_LEVEL_MENU', '');
         
         //
         // Analyze query
@@ -63,6 +73,64 @@ class Loader {
         }
         else {
             // Dynamically fetch template (or display 404)
+            $q_raw = explode('/', $_GET['q']);
+            $q = [];
+            foreach ($q_raw as $v) {
+                if (strlen($v) > 0) {
+                    $q[] = $v;
+                }
+            }
+            
+            // Check what was returned
+            if (count($q) == 0) {
+                // Return 404
+                $this->return404();
+            }
+            else if (count($q) == 1) {
+                // Root template, check if exists
+                if (file_exists(TEMPLATE_DIR . '/' . $q[0] . '.tpl')) {
+                    // Template exists, fetch it
+                    $this->template->assign('TOP_LEVEL_MENU', $q[0]);
+                    $this->template->display($q[0] . '.tpl');
+                }
+                else {
+                    // No template named this, check if directory
+                    if (is_dir(TEMPLATE_DIR . '/' . $q[0])) {
+                        // Is directory, fetch index.tpl within that directory
+                        $this->template->assign('TOP_LEVEL_MENU', $q[0]);
+                        $this->template->display($q[0] . '/index.tpl');
+                    }
+                    else {
+                        // Is not a directory, just return 404
+                        $this->return404();
+                   }
+                }
+            }
+            else {
+                // Rebuild query
+                $query = implode('/', $q);
+                
+                if (file_exists(TEMPLATE_DIR . '/' . $query . '.tpl')) {
+                    // Template exists, fetch it
+                    $this->template->assign('TOP_LEVEL_MENU', $q[0]);
+                    $this->template->assign('SECOUND_LEVEL_MENU', $q[count($q) - 1]);
+                    $this->template->display($query . '.tpl');
+                }
+                else {
+                    // No template named this, check if directory
+                    if (is_dir(TEMPLATE_DIR . '/' . $query)) {
+                        // Is directory, fetch index.tpl within that directory
+                        $this->template->assign('TOP_LEVEL_MENU', $q[0]);
+                        $this->template->assign('SECOUND_LEVEL_MENU', $q[count($q) - 1]);
+                        
+                        $this->template->display($query . '/index.tpl');
+                    }
+                    else {
+                        // Is not a directory, just return 404
+                        $this->return404();
+                   }
+                }
+            }
         }
     }
     
